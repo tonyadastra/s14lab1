29868fd2ddca25245e925d0da7c55df95a753a28
Tony A. Liu <tonyliu@New-Yorker.lan>
2020-06-25 18:20:17 -0400

    Init commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    bestClassEver = 'Best Class Ever'
+    return render_template('index.html', bCE=bestClassEver)
+
+
+@app.route('/world')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/<you>')
+def hello_you(you):
+    return f'Hello, {you}!'
